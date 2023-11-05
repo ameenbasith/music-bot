7581f182ac5b00e9a4c6ffda3f0057586e3a722a
Ameen <ameenbasith2000@gmail.com>
2023-11-04 22:27:00 -0700

    COMPLETE CODE

--- main.py
+++ main.py
@@ -1,38 +1,16 @@
 import asyncio
 import discord
 from discord.ext import commands
-import youtube_dl
+import yt_dlp
+
+discord.opus.load_opus('/opt/homebrew/bin/opusenc')
 
 # Define your Discord token here
-DISCORD_TOKEN = "token"
+DISCORD_TOKEN = "INSERT TOKEN"
 
 # Initialize the bot with specified intents
 intents = discord.Intents().all()
 bot = commands.Bot(command_prefix='!', intents=intents)
-
-youtube_dl.utils.bug_reports_message = lambda: ''
-
-ytdl_format_options = {
-    'format': 'bestaudio/best',
-    'restrictfilenames': True,
-    'noplaylist': True,
-    'nocheckcertificate': True,
-    'ignoreerrors': False,
-    'logtostderr': False,
-    'quiet': True,
-    'no_warnings': True,
-    'default_search': 'auto',
-    'source_address': '0.0.0.0',  # bind to ipv4 since ipv6 addresses cause issues sometimes
-    'noupdate': True  # Add this line to bypass the "Unable to extract uploader id" error
-}
-
-
-
-ffmpeg_options = {
-    'options': '-vn'
-}
-
-ytdl = youtube_dl.YoutubeDL(ytdl_format_options)
 
 class YTDLSource(discord.PCMVolumeTransformer):
     def __init__(self, source, *, data, volume=0.5):
@@ -40,16 +18,6 @@ class YTDLSource(discord.PCMVolumeTransformer):
         self.data = data
         self.title = data.get('title')
         self.url = ""
-
-    @classmethod
-    async def from_url(cls, url, *, loop=None, stream=False):
-        loop = loop or asyncio.get_event_loop()
-        data = await loop.run_in_executor(None, lambda: ytdl.extract_info(url, download=not stream))
-        if 'entries' in data:
-            # take first item from a playlist
-            data = data['entries'][0]
-        filename = data['title'] if stream else ytdl.prepare_filename(data)
-        return filename
 
 @bot.command(name='play', help='To play a song')
 async def play(ctx, url):
@@ -62,9 +30,9 @@ async def play(ctx, url):
             'noplaylist': True,
         }
 
-        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-            info_dict = ydl.extract_info(url, download=False)
-            url2 = info_dict.get("url")
+        ydl = yt_dlp.YoutubeDL(ydl_opts)
+        info_dict = ydl.extract_info(url, download=False)
+        url2 = info_dict.get("url")
 
         if voice_channel.is_playing():
             voice_channel.stop()
@@ -99,4 +67,3 @@ async def stop(ctx):
 
 if __name__ == "__main__":
     bot.run(DISCORD_TOKEN)
-
